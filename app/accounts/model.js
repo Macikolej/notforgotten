a87345f3c18cad334e5c5f520f98f71727139f76
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2017-04-01 21:45:26 +0200

    Add a working sign up feature

--- app/accounts/model.js
+++ app/accounts/model.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const DB = require('../services').DB;
 
 class Account {
@@ -18,6 +19,20 @@ class Account {
     DB.connection.query(
       'SELECT * FROM accounts WHERE id = ?',
       [id],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error, null);
+        }
+        return callback(null, results[0]);
+      }
+    );
+  }
+
+  static create(name, email, password, callback) {
+    const account = { name, email, password: crypto.createHash('sha1').update(password).digest('hex') };
+    DB.connection.query(
+      'INSERT INTO accounts SET ?',
+      account,
       (error, results, fields) => {
         if (error) {
           return callback(error, null);
