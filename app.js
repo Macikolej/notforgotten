047072076e30a36a83065c59a0b03eb7e357d1b9
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2017-04-01 09:25:11 +0200

    Start implementing a better setup

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
+require('dotenv').config();
+const app = require('./app');
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {});
-
-app.listen(3000, () => {
-  console.log('Server running on port localhost:3000')
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server is listening on localhost:${port}...`);
 });
