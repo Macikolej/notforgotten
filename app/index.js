62a8415c7c21fde4ac2eef83daaf20e9710e7a02
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2017-04-01 17:01:31 +0200

    Add SCSS support, refactor the home view

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const sassMiddleware = require('node-sass-middleware');
 const MySQLStore = require('express-mysql-session')(session);
 const routes = require('./routes');
 const config = require('../config');
@@ -17,11 +19,21 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'stylesheets'),
+  dest: path.join(__dirname, '../assets'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/assets'
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'default' }));
 app.set('view engine', 'handlebars');
+
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
 app = routes(app);
 module.exports = app;
--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,6 @@ const path = require('path');
 const routes = (app) => {
   app = require('./static_pages').routes(app);
   app = require('./accounts').routes(app);
-  app.get(/^(.+)$/, function(req, res) {
-    console.log('static file request : ' + req.params[0]);
-    res.sendFile(path.join(__dirname, '../assets', req.params[0]));
-  });
   return app;
 }
 
